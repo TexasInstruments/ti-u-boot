TexasInstruments/ti-u-boot

commit f19cf9d2baed952f191eefe0c5968d0b801d8d4b

FROMLIST: ram: k3-ddrss: Add support for a partial inline ECC region
Instead of defaulting to choosing the entire DDR region when enabling
inline ECC, allow picking of a range within the DDR space using DT to
enable.

It expects such a node within the memory node, in the absence of which
we resort to enabling inline ECC for the entire DDR region:

inline_ecc: protected@9e780000 {
        device_type = "ecc";
        reg = <0x9e780000 0x0080000>;
        bootph-all;
};

Signed-off-by: Neha Malcom Francis <n-francis@ti.com>

drivers/ram/k3-ddrss/k3-ddrss.c
@@ -155,6 +155,7 @@ struct k3_ddrss_desc {
 	lpddr4_obj *driverdt;
 	lpddr4_config config;
 	lpddr4_privatedata pd;
+	struct k3_ddrss_ecc_region ecc_range;
 	struct k3_ddrss_ecc_region ecc_regions[K3_DDRSS_MAX_ECC_REGIONS];
 	u64 ecc_reserved_space;
 	u64 ddr_bank_base[CONFIG_NR_DRAM_BANKS];
@@ -738,6 +739,30 @@ static void k3_ddrss_ddr_reg_init(struct k3_ddrss_desc *ddrss)
 	writel(DDRSS_ECC_CTRL_REG_DEFAULT, ddrss->ddrss_ss_cfg + DDRSS_ECC_CTRL_REG);
 }
 
+static void k3_ddrss_ddr_inline_ecc_base_size_calc(struct k3_ddrss_ecc_region *range)
+{
+	fdt_addr_t base;
+	fdt_size_t size;
+	ofnode node1;
+
+	node1 = ofnode_null();
+
+	do {
+		node1 = ofnode_by_prop_value(node1, "device_type", "ecc", 4);
+	} while (!ofnode_is_enabled(node1));
+
+	base = ofnode_get_addr_size(node1, "reg", &size);
+
+	if (base == FDT_ADDR_T_NONE) {
+		debug("%s: Failed to get ECC node reg and size\n", __func__);
+		range->start = 0;
+		range->range = 0;
+	} else {
+		range->start = base;
+		range->range = size;
+	}
+}
+
 static void k3_ddrss_lpddr4_ecc_calc_reserved_mem(struct k3_ddrss_desc *ddrss)
 {
 	fdtdec_setup_mem_size_base_lowest();
@@ -787,8 +812,11 @@ static void k3_ddrss_lpddr4_ecc_init(struct k3_ddrss_desc *ddrss)
 
 static int k3_ddrss_probe(struct udevice *dev)
 {
+	u64 end;
 	int ret;
 	struct k3_ddrss_desc *ddrss = dev_get_priv(dev);
+	__maybe_unused struct k3_ddrss_data *ddrss_data = (struct k3_ddrss_data *)dev_get_driver_data(dev);
+	__maybe_unused struct k3_ddrss_ecc_region *range = &ddrss->ecc_range;
 
 	debug("%s(dev=%p)\n", __func__, dev);
 
@@ -826,9 +854,27 @@ static int k3_ddrss_probe(struct udevice *dev)
 
 		k3_ddrss_lpddr4_ecc_calc_reserved_mem(ddrss);
 
-		/* Always configure one region that covers full DDR space */
-		ddrss->ecc_regions[0].start = 0;
-		ddrss->ecc_regions[0].range = ddrss->ddr_ram_size - ddrss->ecc_reserved_space;
+		k3_ddrss_ddr_inline_ecc_base_size_calc(range);
+		if (!range->range) {
+			/* Configure entire DDR space by default */
+			debug("%s: Defaulting to protecting entire DDR space using inline ECC\n",
+			      __func__);
+			ddrss->ecc_range.start = ddrss->ddr_bank_base[0];
+			ddrss->ecc_range.range = ddrss->ddr_ram_size - ddrss->ecc_reserved_space;
+		} else {
+			ddrss->ecc_range.start = range->start;
+			ddrss->ecc_range.range = range->range;
+		}
+
+		end = ddrss->ecc_range.start + ddrss->ecc_range.range;
+
+		if (end > (ddrss->ddr_ram_size - ddrss->ecc_reserved_space))
+			ddrss->ecc_regions[0].range = ddrss->ddr_ram_size - ddrss->ecc_reserved_space;
+		else
+			ddrss->ecc_regions[0].range = ddrss->ecc_range.range;
+
+		ddrss->ecc_regions[0].start = ddrss->ecc_range.start - ddrss->ddr_bank_base[0];
+
 		k3_ddrss_lpddr4_ecc_init(ddrss);
 	}
 

commit ea09c8fdc98285f8359e5faa2d4372fd58aa4165

PENDING: ram: k3-ddrss: fix ecc range checking bug
The original checking is wrong, it basically compares memory address

    end = ddrss->ecc_range.start + ddrss->ecc_range.range;

with memory size:

    if (end > (ddr_ram_size - ecc_res))

so the if() condition would be always true. This causes the ECC config
never take input from the devicetree.

Fixes: f19cf9d2baed ("FROMLIST: ram: k3-ddrss: Add support for a partial inline ECC region")

Signed-off-by: Bin Liu <b-liu@ti.com>
Reviewed-by: Udit Kumar <u-kumar1@ti.com>

drivers/ram/k3-ddrss/k3-ddrss.c
@@ -1360,11 +1360,11 @@ static void k3_ddrss_run_retention_latch_clear_sequence(struct k3_ddrss_desc *dd
 
 static int k3_ddrss_probe(struct udevice *dev)
 {
-	u64 end, bank0, bank1;
+	u64 bank0, bank1;
 	int ret;
 	struct k3_ddrss_desc *ddrss = dev_get_priv(dev);
 	__maybe_unused u32 inst;
-	__maybe_unused u64 ddr_ram_size, ecc_res, start;
+	__maybe_unused u64 ddr_ram_size, ecc_res, start, end;
 	__maybe_unused struct k3_ddrss_data *ddrss_data = (struct k3_ddrss_data *)dev_get_driver_data(dev);
 	__maybe_unused struct k3_ddrss_ecc_region *range = &ddrss->ecc_range;
 	__maybe_unused struct k3_msmc *msmc_parent = NULL;
@@ -1445,13 +1445,12 @@ static int k3_ddrss_probe(struct udevice *dev)
 		}
 
 #if !CONFIG_IS_ENABLED(K3_MULTI_DDR)
-		end = ddrss->ecc_range.start + ddrss->ecc_range.range;
 		start = ddrss->ecc_range.start;
 		inst = ddrss->instance;
 		ddr_ram_size = ddrss->ddr_ram_size;
 		ecc_res = ddrss->ecc_reserved_space;
 
-		if (end > (ddr_ram_size - ecc_res))
+		if (ddrss->ecc_range.range > (ddr_ram_size - ecc_res))
 			ddrss->ecc_regions[0].range = ddr_ram_size - ecc_res;
 		else
 			ddrss->ecc_regions[0].range = ddrss->ecc_range.range;