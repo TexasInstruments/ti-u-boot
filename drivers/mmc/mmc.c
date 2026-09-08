1463e6d36b2ea1f5a6a189afee922cdac247103a
Judith Mendez <jm@ti.com>
2026-09-08 17:16:25 -0500

    FROMLIST: mmc: core: Add power-off-delay-us support for slow RC discharge
    Add support for power-off-delay-us property to allow additional delay
    after mmc_power_off(). Boards with slow RC discharge on MMC VDD rails
    can use this to fully discharge the card before power-on.
    
    Default delay is 0 (no additional delay). Boards requiring longer
    discharge can override via DT property power-off-delay-us in microseconds.
    
    link: https://lore.kernel.org/u-boot/20260908201146.532645-1-jm@ti.com/
    Signed-off-by: Judith Mendez <jm@ti.com>

--- drivers/mmc/mmc.c
+++ drivers/mmc/mmc.c
@@ -2895,6 +2895,8 @@ static int mmc_power_on(struct mmc *mmc)
 
 static int mmc_power_off(struct mmc *mmc)
 {
+	u32 delay_us = 0;
+
 	mmc_set_clock(mmc, 0, MMC_CLK_DISABLE);
 #if CONFIG_IS_ENABLED(DM_MMC) && CONFIG_IS_ENABLED(DM_REGULATOR)
 	if (mmc->vmmc_supply) {
@@ -2916,6 +2918,12 @@ static int mmc_power_off(struct mmc *mmc)
 			return ret;
 		}
 	}
+
+	if (mmc->dev)
+		dev_read_u32(mmc->dev, "power-off-delay-us", &delay_us);
+
+	if (delay_us)
+		udelay(delay_us);
 #endif
 	return 0;
 }
